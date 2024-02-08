ffb1f1e70254469ed46199bef9965e238cd132bd
brobo1 <armanahz95@gmail.com>
2024-02-08 09:18:39 +0100

    Add 'gh-pages' dependency and post-build script for deployment
    The 'gh-pages' package is added as a dependency in 'package.json'. A 'postbuild' script is added to deploy the build output to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '127.0.0.1',
-  },
-  base: '/brobo1.github.io/',
+	plugins: [react()],
+	base: '/brobo1.github.io/',
 })
