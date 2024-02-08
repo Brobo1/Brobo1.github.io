6ed549215433ccbcd0efe6d70f0f92f5ef380072
brobo1 <armanahz95@gmail.com>
2024-02-08 14:46:41 +0100

    Remove redundant asset index-hAbGgr1X.js
    This commit deletes a redundant JavaScript asset file, index-hAbGgr1X.js. This file contained code related to various React components and was not required in the current build. The removal of this file helps reduce the overall bundle size.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: process.env.NODE_ENV === 'production' ? '/brobo1.github.io/' : '/',
+	base: process.env.NODE_ENV === 'production' ? '/dist/' : '/',
 })
